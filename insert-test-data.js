33d1c34ff837c2f832402bc08a42bdf3ee4504c5
Henri Kinnunen <henri@smartly.io>
2015-11-21 22:27:12 +0200

    Add ratings to test data

--- insert-test-data.js
+++ insert-test-data.js
@@ -1,62 +1,77 @@
 db.articles.insert([
     {
         title: "Yes, Republicans Are Being Cowardly in Rejecting Syrian Refugees",
-        link: "http://www.slate.com/articles/news_and_politics/politics/2015/11/republicans_are_being_cowardly_in_rejecting_syrian_refugees_they_deserve.html"
+        link: "http://www.slate.com/articles/news_and_politics/politics/2015/11/republicans_are_being_cowardly_in_rejecting_syrian_refugees_they_deserve.html",
+        rating: 1
     },
     {
         title: "EU leaders leave refugees out in the cold",
-        link: "http://socialistworker.org/2015/11/05/eu-leaders-leave-refugees-out"
+        link: "http://socialistworker.org/2015/11/05/eu-leaders-leave-refugees-out",
+        rating: 1
     },
     {
         title: "ISIS Wants You to Hate Muslims",
-        link: "http://www.thenation.com/article/assault-on-life/"
+        link: "http://www.thenation.com/article/assault-on-life/",
+        rating: 1
     },
     {
         title: "Canadian couple cancels wedding to help Syrian refugees",
-        link: "http://www.bbc.com/news/world-us-canada-34883292"
+        link: "http://www.bbc.com/news/world-us-canada-34883292",
+        rating: 2
     },
     {
         title: "The law is clear: States cannot reject Syrian refugees",
-        link: "https://www.washingtonpost.com/posteverything/the-law-is-clear-states-cannot-reject-syrian-refugees/2015/11/19/1525b555-a35f-4c63-a3bd-e3ff66a2af59_story.html"
+        link: "https://www.washingtonpost.com/posteverything/the-law-is-clear-states-cannot-reject-syrian-refugees/2015/11/19/1525b555-a35f-4c63-a3bd-e3ff66a2af59_story.html",
+        rating: 2
     },
     {
         title: "UN: Over 1 million child refugees from Syrian war",
-        link: "http://www.politico.com/story/2013/08/syria-un-one-million-child-refugees-syrian-war-095841"
+        link: "http://www.politico.com/story/2013/08/syria-un-one-million-child-refugees-syrian-war-095841",
+        rating: 2
     },
     {
         title: "Syrian Refugee Issue Still Divisive",
-        link: "http://abcnews.go.com/WNT/video/syrian-refugee-issue-divisive-35292781"
+        link: "http://abcnews.go.com/WNT/video/syrian-refugee-issue-divisive-35292781",
+        rating: 3
     },
     {
         title: "Syrian refugee crisis: It's about compassion and security",
-        link: "http://edition.cnn.com/2015/11/20/opinions/smith-syrian-refugee-crisis/index.html"
+        link: "http://edition.cnn.com/2015/11/20/opinions/smith-syrian-refugee-crisis/index.html",
+        rating: 3
     },
     {
         title: "Syrians in America: How Safe Is the U.S. Refugee Program?",
-        link: "http://www.nbcnews.com/storyline/paris-terror-attacks/how-safe-u-s-refugee-program-n465841"
+        link: "http://www.nbcnews.com/storyline/paris-terror-attacks/how-safe-u-s-refugee-program-n465841",
+        rating: 3
     },
     {
         title: "Terrorists who Killed American Soldiers were Legally Admitted into the Country via Refugee Program",
-        link: "http://www.youngcons.com/terrorists-who-killed-american-soldiers-were-legally-admitted-into-the-country-via-refugee-program/"
+        link: "http://www.youngcons.com/terrorists-who-killed-american-soldiers-were-legally-admitted-into-the-country-via-refugee-program/",
+        rating: 4
     },
     {
         title: "Most Americans want to keep Syrian refugees out of the country",
-        link: "http://nypost.com/2015/11/19/most-americans-want-to-keep-syrian-refugees-out-of-the-country/"
+        link: "http://nypost.com/2015/11/19/most-americans-want-to-keep-syrian-refugees-out-of-the-country/",
+        rating: 4
     },
     {
         title: "Paris Attacks Shine Light on Europe’s Failing Border Policies",
-        link: "http://www.wsj.com/articles/paris-attacks-shine-light-on-europes-failing-border-policies-1447976231"
+        link: "http://www.wsj.com/articles/paris-attacks-shine-light-on-europes-failing-border-policies-1447976231",
+        rating: 4
     },
     {
         title: "Rick Santorum: Taking Refugees Gives ISIS What They Want",
-        link: "http://www.newsmax.com/Newsmax-Tv/rick-santorum-isis-isil-syrian-refugees/2015/11/19/id/702910/"
+        link: "http://www.newsmax.com/Newsmax-Tv/rick-santorum-isis-isil-syrian-refugees/2015/11/19/id/702910/",
+        rating: 5
     },
     {
         title: "Son of Holocaust survivor: Don't let Syrian refugees in to US",
-        link: "http://www.foxnews.com/world/2015/11/19/son-holocaust-survivor-dont-let-syrian-refugees-in/"
+        link: "http://www.foxnews.com/world/2015/11/19/son-holocaust-survivor-dont-let-syrian-refugees-in/",
+        rating: 5
     },
     {
         title: "Paris Attacks Shine Light on Europe’s Failing Border Policies",
-        link: "http://www.wsj.com/articles/paris-attacks-shine-light-on-europes-failing-border-policies-1447976231"
+        link: "http://www.wsj.com/articles/paris-attacks-shine-light-on-europes-failing-border-policies-1447976231",
+        rating: 5
     }
 ]);
