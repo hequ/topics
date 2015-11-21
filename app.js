388ea26efa3bc913633276ba0845fa48d2cc3c80
Henri Kinnunen <henri@smartly.io>
2015-11-21 21:34:34 +0200

    add update route

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ var express = require('express');
 var Q = require("q");
 var request = require('request');
 var _ = require('lodash');
-var rssParser = require('rss-parse-promise');
+var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-
-
-
+var ObjectId = require('mongodb').ObjectID;
 var app = express();
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
 var url = 'mongodb://localhost:27017/development';
 
-app.get('/api/news', function (req, res) {
+app.post('/api/news', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        var articles = db.collection("articles");
+
+        articles.updateOne({_id: ObjectId(req.body.id)}, {$set: {'votes': req.body.vote}}, function(err, art) {
+            res.send(art);
+        });
+    });
+});
+
+app.get('/api/news', function(req, res) {
     MongoClient.connect(url, function(err, db) {
         var articles = db.collection("articles");
 
