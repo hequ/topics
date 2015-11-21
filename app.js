f9b869592148d85ccb8776bfaf9dbbbdd1562587
Henri Kinnunen <henri@smartly.io>
2015-11-21 14:30:40 +0200

    serve public

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var Q = require("q");
 var request = require('request');
 var _ = require('lodash');
+var rssParser = require('rss-parse-promise');
 
 var app = express();
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    var rssParser = require('rss-parse-promise');
-    rssParser('http://www.echojs.com/rss').then(function(value) {
-       res.send(value);
+app.get('/news', function (req, res) {
+    rssParser('http://rss.cnn.com/rss/edition_world.rss').then(function (value) {
+        res.send(_.pluck(value, 'title'));
     });
 });
 
