bf896d21c7690e759ba7f7daf81e5445bd29d8e8
Henri Kinnunen <henri@smartly.io>
2015-11-21 14:21:08 +0200

    more node stuff

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var Q = require("q");
+var request = require('request');
+var _ = require('lodash');
+
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    var rssParser = require('rss-parse-promise');
+    rssParser('http://www.echojs.com/rss').then(function(value) {
+       res.send(value);
+    });
 });
 
 var server = app.listen(3000, function () {
