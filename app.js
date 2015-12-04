32604d78d2be0f4b8d7926eb4a1b5e53fd202d6d
Anssi Kinnunen <aatkin@utu.fi>
2015-12-04 22:32:07 +0200

    this is getting tedious. come on

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(80, function () {
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
   var host = server.address().address;
-  var port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 });
