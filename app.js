dde25c09fe85ec2ab5d26381a9abbd2e96413020
Anssi Kinnunen <aatkin@utu.fi>
2015-12-04 22:26:52 +0200

    move from port 3000 to 80.. for heroku

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
